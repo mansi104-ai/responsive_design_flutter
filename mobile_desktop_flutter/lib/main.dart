cb1f3d7934986b05d3bf07d66000e1cb363c7801
mansi104-ai <118623172+mansi104-ai@users.noreply.github.com>
2023-07-13 14:48:08 +0530

    routes problem to be resolved

--- mobile_desktop_flutter/lib/main.dart
+++ mobile_desktop_flutter/lib/main.dart
@@ -1,17 +1,133 @@
 import 'package:flutter/material.dart';
 
-void main(){
-  runApp(const MyApp());
-}
+void main() => runApp(const SignUpApp());
 
-class MyApp extends StatelessWidget{
-  const MyApp({Key? key}): super(key: key);
-  
+class SignUpApp extends StatelessWidget{
+  const SignUpApp();
+
+ 
+
   @override
   Widget build(BuildContext context){
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomePage(),
-    ); // MaterialApp
+      routes:{
+        '/':(context) => const SignUpScreen(),
+        '/welcome': (context) => const WelcomeScreen(),
+      } ,
+    );
   }
 }
+
+
+class SignUpScreen extends StatelessWidget{
+  const SignUpScreen();
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.grey[1000],
+      body: const Center(
+        child: SizedBox(
+          width: 400,
+          child: Card(
+            child: SignUpForm(),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
+class WelcomeScreen extends StatelessWidget {
+  const WelcomeScreen();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child:
+            Text('Welcome!', style: Theme.of(context).textTheme.displayMedium),
+      ),
+    );
+  }
+}
+
+
+class SignUpForm extends StatefulWidget{
+  const SignUpForm();
+
+  @override 
+  State<SignUpForm> createState() => _SignUpFormState();
+}
+
+class _SignUpFormState extends State<SignUpForm>{
+  final _firstNameTextController = TextEditingController();
+  final _lastNameTextController  = TextEditingController();
+  final _usernameTextController  = TextEditingController();
+
+  double _formProgress = 0;
+void _showWelcomeScreen() {
+  Navigator.of(context).pushNamed('/welcome');
+  };
+
+  @override
+
+
+  Widget build(BuildContext context){
+    return Form(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          LinearProgressIndicator(value: _formProgress),
+          Text('Sign Up', style: Theme.of(context).textTheme.headlineMedium),
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: TextFormField(
+              controller: _firstNameTextController,
+              decoration: const InputDecoration(hintText: 'First Name')
+              ),
+            ),
+
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: TextFormField(
+              controller: _lastNameTextController,
+              decoration: const InputDecoration(hintText: 'Last Name'),
+
+              ),
+            ),
+          
+          Padding(
+            padding: const EdgeInsets.all(20),
+            child: TextFormField(
+              controller: _usernameTextController,
+              decoration: InputDecoration(hintText: 'User Name'),
+              ),
+            ),
+
+          TextButton(
+            style: ButtonStyle(
+              foregroundColor: MaterialStateProperty.resolveWith(
+                (Set <MaterialState> states){
+                  return states.contains(MaterialState.disabled)
+                      ? null 
+                      : Colors.blue;
+
+                }),
+             ),
+            onPressed: _showWelcomeScreen,
+            child: const Text('Sign Up'),
+            ),
+
+
+        ],
+
+      ) ,
+    );
+  }
+
+}
+
+
+
